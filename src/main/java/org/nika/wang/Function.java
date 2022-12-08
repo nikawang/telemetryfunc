4920601a1a74ee3de620e05943c84e6d594a433e
DanielWangZhanggui <danielwzhg@gmail.com>
2022-12-08 22:14:55 +0800

    app resources

--- src/main/java/org/nika/wang/Function.java
+++ src/main/java/org/nika/wang/Function.java
@@ -88,8 +88,8 @@ public class Function {
                     TelemetryItem item,
             @CosmosDBOutput(
                     name = "databaseOutput",
-                    databaseName = "TelemetryDb",
-                    collectionName = "TelemetryInfo",
+                    databaseName = "",
+                    collectionName = "",
                     connectionStringSetting = "CosmosDBConnectionString")
                     OutputBinding<TelemetryItem> document,
             final ExecutionContext context) {
